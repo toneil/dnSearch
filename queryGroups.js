ed9663abf366f5ace1bdecaff5b7c91c270a992d
Tobias Neil <brortobiasneil@gmail.com>
2016-09-01 11:39:15 +0200

    Language specific search terms

--- queryGroups.js
+++ queryGroups.js
@@ -1,23 +1,24 @@
-module.exports =
-[
-    {
-        title: "Syrien",
-        terms: ["Syrien", "syrier", "syrisk"]
-    },
-    {
-        title: "Afghanistan",
-        terms: ["Afghanistan", "afghan"]
-    },
-    {
-        title: "Etiopien",
-        terms: ["Etiopien", "etiop"]
-    },
-    {
-        title: "Somalia",
-        terms: ["Somalia", "somalier", "somalisk"]
-    },
-    {
-        title: "Irak",
-        terms: ["Irak", "irak"]
-    }
-]
+module.exports = {
+    "swe": [
+        {
+            title: "Syria",
+            terms: ["Syrien", "syrier", "syrisk"]
+        },
+        {
+            title: "Afghanistan",
+            terms: ["Afghanistan", "afghan"]
+        },
+        {
+            title: "Ethiopia",
+            terms: ["Etiopien", "etiop"]
+        },
+        {
+            title: "Somalia",
+            terms: ["Somalia", "somalier", "somalisk"]
+        },
+        {
+            title: "Iraq",
+            terms: ["Irak", "irak"]
+        }
+    ],
+}
